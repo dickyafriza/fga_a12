94a87c722d6af1c15df40b4edb7f79b90d55a347
Firmansyahreza03 <fmuhammadreza03@gmail.com>
2022-03-15 02:29:29 +0700

    update punya reza

---
+++ script.js
@@ -0,0 +1,39 @@
+const order1 = document.getElementsByClassName("order-1");
+const order2 = document.getElementsByClassName("order-2");
+const order3 = document.getElementsByClassName("order-3");
+
+const orderHandler = (value) => {
+  console.log(`Order Toyota ${value}`);
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("user-testimoni");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
