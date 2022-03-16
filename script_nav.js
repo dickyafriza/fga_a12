03991d70fa3a23eddce8c22d701c427085c188da
afriza <afrizadicky6@gmail.com>
2022-03-16 11:23:52 +0700

    banner_responsive

--- script_nav.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('toggle-Button');
-const navilist = document.getElementById('navi-list');
-
-toggleButton.addEventListener('click', () => {
-    navilist.classList.toggle('active');
-})
